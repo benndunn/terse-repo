f4cc725d4d7df093e446919a25bed955108b642b
Benn Dunn <BennDunn83@gmail.com>
2025-06-10 16:03:04 -0600

    fix embarrassing typo and add typer

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
-def main(who:str="world"):
-  print("Hello, {who}."}
-        
+import typer
+from typing import Optional
+
+def main(who:Optional[str]=typer.Option("world", help="Who to say hello to")):
+  
+  print(f"Hello, {who}.")
+
 if __name__=="__main__":
-  main()
+  typer.run(main)
+        
